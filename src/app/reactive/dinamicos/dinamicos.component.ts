8d5009aeeff7702ddafabb9342b253e2c552f832
goncalveshigino <goncalveshigino20@gmail.com>
2021-09-21 07:43:24 +0100

    feito

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -11,15 +11,42 @@ export class DinamicosComponent  {
 
   miFormulario: FormGroup = this.fb.group({
 
-    nome: ['', [Validators.minLength(3), Validators.required]]
-  })
+    nome: ['', [Validators.minLength(3), Validators.required]],
+    favoritos: this.fb.array([
+      ['Metal Gear', Validators.required],
+      ['Death Stranding', Validators.required]
+    ], Validators.required)
+  });
+
+  novoFavorito: FormControl = this.fb.control('', Validators.required);
+
+  get favoritosArr() {
+    return this.miFormulario.get('favoritos') as FormArray;
+  }
 
   validarCampos( campo: string) {
      return this.miFormulario.controls[campo].errors
       && this.miFormulario.controls[campo].touched;
   }
 
-  constructor( private fb: FormBuilder) { }
+  adicionarFavorito() {
+    
+    if (this.novoFavorito.invalid) { return; }
+
+    // this.favoritosArr.push(new FormControl(this.novoFavorito.value, Validators.required)),
+    this.favoritosArr.push(this.fb.control(
+      this.novoFavorito.value, Validators.required
+    ))
+      
+      this.novoFavorito.reset();
+  }
+
+  constructor(private fb: FormBuilder) { }
+  
+  eliminar(i: number) {
+    this.favoritosArr.removeAt(i);
+  }
+
 
   salvar() {
     
